e5c7e60f81137f0745fd87b87a60bf122023308d
Thatayotlhe Tsenang <tsenangthatayotlhe04@gmail.com>
2026-06-10 10:58:05 +0200

    Deploy Next app to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
+const nextConfig = {
+  ...(isGitHubPages
+    ? {
+        output: "export",
+        basePath: "/BOCRA",
+        assetPrefix: "/BOCRA/",
+        images: {
+          unoptimized: true,
+        },
+      }
+    : {}),
+};
 
 module.exports = nextConfig;
